4538289b9ff3a1e9cb6fbcb258a74e47f17eeac6
dedenys <dedenys.school@gmail.com>
2025-02-13 18:37:16 -0700

    making QueenMovesCalculator code cleaner

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -15,6 +15,7 @@ public class QueenMovesCalculator implements PieceMoveCalculator{
         this.pieceColor = pieceColor;
     }
 
+
     @Override
     public Collection<ChessMove> pieceMoves() {
 
@@ -22,186 +23,8 @@ public class QueenMovesCalculator implements PieceMoveCalculator{
         int row = position.getRow();
         int column = position.getColumn();
 
-        // #### straight movement ######
-
-        // left movement
-        ChessPosition left = new ChessPosition(row, column-1);
-        while (outOfBoundsCheck(left)) {
-
-            if (board.getPiece(left) == null) {
-                ChessMove m = new ChessMove(position, left, null);
-                al.add(m);
-
-                left = new ChessPosition(row, left.getColumn()-1);
-            }
-            else if (board.getPiece(left) != null && board.getPiece(left).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, left, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // right movement
-        ChessPosition right = new ChessPosition(row, column+1);
-        while (outOfBoundsCheck(right)) {
-
-            if (board.getPiece(right) == null) {
-                ChessMove m = new ChessMove(position, right, null);
-                al.add(m);
-
-                right = new ChessPosition(row, right.getColumn()+1);
-            }
-            else if (board.getPiece(right) != null && board.getPiece(right).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, right, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // up movement
-
-        ChessPosition up = new ChessPosition(row+1, column);
-        while (outOfBoundsCheck(up)) {
-
-            if (board.getPiece(up) == null) {
-                ChessMove m = new ChessMove(position, up, null);
-                al.add(m);
-
-                up = new ChessPosition(up.getRow()+1, column);
-            }
-            else if (board.getPiece(up) != null && board.getPiece(up).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, up, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-        // down movement
-
-        ChessPosition down = new ChessPosition(row-1, column);
-        while (outOfBoundsCheck(down)) {
-
-            if (board.getPiece(down) == null) {
-                ChessMove m = new ChessMove(position, down, null);
-                al.add(m);
-
-                down = new ChessPosition(down.getRow()-1, column);
-            }
-            else if (board.getPiece(down) != null && board.getPiece(down).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, down, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // #### diagonal movement ####
-
-        // left up movement
-        ChessPosition leftUp = new ChessPosition(row+1, column-1);
-        while (outOfBoundsCheck(leftUp)) {
-
-            if (board.getPiece(leftUp) == null) {
-                ChessMove m = new ChessMove(position, leftUp, null);
-                al.add(m);
-
-                leftUp = new ChessPosition(leftUp.getRow()+1, leftUp.getColumn()-1);
-            }
-            else if (board.getPiece(leftUp) != null && board.getPiece(leftUp).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, leftUp, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // left down movement
-        ChessPosition leftDown = new ChessPosition(row-1, column-1);
-        while (outOfBoundsCheck(leftDown)) {
-
-            if (board.getPiece(leftDown) == null) {
-                ChessMove m = new ChessMove(position, leftDown, null);
-                al.add(m);
-
-                leftDown = new ChessPosition(leftDown.getRow()-1, leftDown.getColumn()-1);
-            }
-            else if (board.getPiece(leftDown) != null && board.getPiece(leftDown).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, leftDown, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // right up movement
-        ChessPosition rightUp = new ChessPosition(row+1, column+1);
-        while (outOfBoundsCheck(rightUp)) {
-
-            if (board.getPiece(rightUp) == null) {
-                ChessMove m = new ChessMove(position, rightUp, null);
-                al.add(m);
-
-                rightUp = new ChessPosition(rightUp.getRow()+1, rightUp.getColumn()+1);
-            }
-            else if (board.getPiece(rightUp) != null && board.getPiece(rightUp).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, rightUp, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
-
-        // right down movement
-        ChessPosition rightDown = new ChessPosition(row-1, column+1);
-        while (outOfBoundsCheck(rightDown)) {
-
-            if (board.getPiece(rightDown) == null) {
-                ChessMove m = new ChessMove(position, rightDown, null);
-                al.add(m);
-
-                rightDown = new ChessPosition(rightDown.getRow()-1, rightDown.getColumn()+1);
-            }
-            else if (board.getPiece(rightDown) != null && board.getPiece(rightDown).getTeamColor() != pieceColor) {
-                ChessMove m = new ChessMove(position, rightDown, null);
-                al.add(m);
-
-                break;
-            }
-            else {
-                break;
-            }
-
-        }
+        striaghtMovement(row, column, al);
+        diagonalMovement(row, column, al);
 
         return al;
     }
@@ -217,5 +40,66 @@ public class QueenMovesCalculator implements PieceMoveCalculator{
         else {
             return true;
         }
+    }
+
+    private void checkPosition(ChessPosition pos, int row, int column, Collection<ChessMove> al, int rowDif, int colDif) {
+        while (outOfBoundsCheck(pos)) {
+
+            if (board.getPiece(pos) == null) {
+                ChessMove m = new ChessMove(position, pos, null);
+                al.add(m);
+
+                pos = new ChessPosition(pos.getRow()+rowDif, pos.getColumn()+colDif);
+            }
+            else if (board.getPiece(pos) != null && board.getPiece(pos).getTeamColor() != pieceColor) {
+                ChessMove m = new ChessMove(position, pos, null);
+                al.add(m);
+
+                break;
+            }
+            else {
+                break;
+            }
+
+        }
+    }
+
+    private void striaghtMovement(int row, int column, Collection<ChessMove> al) {
+        // left movement
+        ChessPosition left = new ChessPosition(row, column - 1);
+        checkPosition(left, row, column, al, 0, -1);
+
+        // right movement
+        ChessPosition right = new ChessPosition(row, column + 1);
+        checkPosition(right, row, column, al, 0, 1);
+
+        // up movement
+
+        ChessPosition up = new ChessPosition(row + 1, column);
+        checkPosition(up, row, column, al, 1, 0);
+        // down movement
+
+        ChessPosition down = new ChessPosition(row - 1, column);
+        checkPosition(down, row, column, al, -1, 0);
+    }
+
+    private void diagonalMovement(int row, int column, Collection<ChessMove> al) {
+        // #### diagonal movement ####
+
+        // left up movement
+        ChessPosition leftUp = new ChessPosition(row+1, column-1);
+        checkPosition(leftUp, row, column, al, 1, -1);
+
+        // left down movement
+        ChessPosition leftDown = new ChessPosition(row-1, column-1);
+        checkPosition(leftDown, row, column, al, -1, -1);
+
+        // right up movement
+        ChessPosition rightUp = new ChessPosition(row+1, column+1);
+        checkPosition(rightUp, row, column, al, 1, 1);
+
+        // right down movement
+        ChessPosition rightDown = new ChessPosition(row-1, column+1);
+        checkPosition(rightDown, row, column, al, -1, 1);
     }
 }
