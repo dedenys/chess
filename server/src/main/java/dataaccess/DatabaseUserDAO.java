1d86ec2390cdc3f769ec3a82e8c3e7c5e1df20ea
dedenys <dedenys.school@gmail.com>
2025-03-10 23:00:11 -0600

    Creating DatabaseUserDAO

--- server/src/main/java/dataaccess/DatabaseUserDAO.java
+++ server/src/main/java/dataaccess/DatabaseUserDAO.java
@@ -1,4 +1,25 @@
 package dataaccess;
 
-public class DatabaseUserDAO {
+import model.UserData;
+
+import java.util.HashMap;
+
+public class DatabaseUserDAO implements UserDAO {
+    final private HashMap<String, UserData> users = new HashMap<>();
+
+    public UserData getUser(String username) {
+        UserData user = users.get(username);
+        return user;
+    }
+    public UserData createUser(UserData user) {
+        users.put(user.username(), user);
+        return user;
+    }
+    public void clear() {
+        users.clear();
+    }
+    public int getLength() {
+        return users.size();
+    }
+
 }
