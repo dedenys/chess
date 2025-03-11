b2f78330cc5f806e4f243638e42971c723ba5e0f
dedenys <dedenys.school@gmail.com>
2025-03-10 22:59:40 -0600

    Creating DatabaseAuthDAO

--- server/src/main/java/dataaccess/DatabaseAuthDAO.java
+++ server/src/main/java/dataaccess/DatabaseAuthDAO.java
@@ -1,4 +1,28 @@
 package dataaccess;
 
-public class DatabaseAuthDAO {
+import model.AuthData;
+
+import java.util.HashMap;
+
+public class DatabaseAuthDAO implements AuthDAO{
+    private final HashMap<String, AuthData> authorizations = new HashMap<>();
+
+    public AuthData getAuth(String authToken) {
+        AuthData auth = authorizations.get(authToken);
+        return auth;
+    }
+    public AuthData createAuth(AuthData auth) {
+        authorizations.put(auth.authToken(), auth);
+        return auth;
+    }
+    public void removeAuth(String authToken) {
+        authorizations.remove(authToken);
+        System.out.println(authorizations);
+    }
+    public void clear() {
+        authorizations.clear();
+    }
+    public int getLength() {
+        return authorizations.size();
+    }
 }
