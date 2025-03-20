85d6494cbbd77e67371325aac194757db6c80580
dedenys <dedenys.school@gmail.com>
2025-03-19 21:24:35 -0600

    Setting up ServerFacade file

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,22 @@
+package server;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+
+
+}
