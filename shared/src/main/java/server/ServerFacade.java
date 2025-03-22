a01091383b8f2acab04b012bcec176c74ff1bc83
dedenys <dedenys.school@gmail.com>
2025-03-21 21:47:45 -0600

    Working on ServerFacade methods

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,8 @@
 package server;
 
 import com.google.gson.Gson;
+import model.GameData;
+import model.UserData;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +19,40 @@ public class ServerFacade {
     }
 
 
+    public UserData register(UserData user) throws Exception {
+        var path = "/user";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public UserData login(UserData user) throws Exception {
+        var path = "/session";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public UserData logout(UserData user) throws Exception {
+        var path = "/session";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public GameData createGame(GameData game) throws Exception {
+        var path = "/game";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public GameData joinGame(int id) throws Exception {
+        var path = "/game";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public GameData[] listGames() throws Exception {
+        var path = "/game";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
+
+    public void clour() throws Exception {
+        var path = "/db";
+        //return this.makeRequest("POST", path, pet, Pet.class);
+    }
 
 
 }
