76d3815a638e297fea1acdf749bf3b9936903ddc
dedenys <dedenys.school@gmail.com>
2025-03-22 20:40:34 -0600

    Adding ChessClient

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,13 @@
+package client;
+
+public class ChessClient {
+
+    public String help() {
+            return """
+                    - help
+                    - register <yourname>
+                    - login <yourname>
+                    - quit
+                    """;
+    }
+}
