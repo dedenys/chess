608dc0944ae0ccc13603617c75d675ccccd98b9b
dedenys <dedenys.school@gmail.com>
2025-03-25 09:24:37 -0600

    Checkered pattern complete

--- client/src/main/java/client/GameClient.java
+++ client/src/main/java/client/GameClient.java
@@ -1,6 +1,8 @@
 package client;
 
+import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPosition;
 import model.GameData;
 import server.ServerFacade;
 
@@ -16,6 +18,11 @@ public class GameClient {
     private final ServerFacade server;
     public GameData gameData;
     public ChessGame game;
+    public String color = "WHITE";
+    public static String[] numbers = {"8","7","6","5","4","3","2","1"};
+    public static String[] letters = {"a","b","c","d","e","f","g","h"};
+    public static ChessGame testGame;
+    public static ChessBoard testBoard;
 
     // UI
 
@@ -36,6 +43,8 @@ public class GameClient {
     public GameClient(String serverUrl) {
         server = new ServerFacade(serverUrl);
         this.serverUrl = serverUrl;
+        testGame = new ChessGame();
+        testBoard = testGame.getBoard();
     }
 
     public String help() {
@@ -67,21 +76,19 @@ public class GameClient {
 
         drawHeaders(out);
 
-        drawTicTacToeBoard(out);
+        drawBoard(out);
 
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_WHITE);
-
-        return "Hello!";
+        setGray(out);
+        return "";
     }
 
     private static void drawHeaders(PrintStream out) {
 
-        setBlack(out);
+        setGray(out);
 
         String[] headers = { "1", "2", "3","4","5","6","7","8" };
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headers[boardCol]);
+            drawHeader(out, letters[boardCol]);
 
             if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
                 out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
@@ -92,58 +99,102 @@ public class GameClient {
     }
 
     private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+       // int prefixLength = 2;//SQUARE_SIZE_IN_PADDED_CHARS / 2;
+       // int suffixLength = 1;//SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
-        out.print(EMPTY.repeat(prefixLength));
+        //out.print(EMPTY.repeat(prefixLength));
+        out.print("      ");
         printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
+        out.print("  ");
+        //out.print(EMPTY.repeat(suffixLength));
     }
 
     private static void printHeaderText(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_BG_COLOR_DARK_GREY);
         out.print(SET_TEXT_COLOR_GREEN);
 
         out.print(player);
 
-        setBlack(out);
+        setGray(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void drawBoard(PrintStream out) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
 
-            drawRowOfSquares(out);
+//            out.print(SET_BG_COLOR_BLACK);
+//            out.print(SET_TEXT_COLOR_GREEN);
+//
+//            out.print(numbers[boardRow]);
+            drawRowOfSquares(out, boardRow);
 
             if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(SET_BG_COLOR_DARK_GREY);
+
+                out.print(SET_TEXT_COLOR_GREEN);
+                out.print("  ");
                 // Draw horizontal row separator.
                 drawHorizontalLine(out);
-                setBlack(out);
+                setGray(out);
             }
         }
     }
 
-    private static void drawRowOfSquares(PrintStream out) {
+    private static void drawRowOfSquares(PrintStream out, int boardRow) {
         boolean on = false;
+        boolean firstRow = true;
+
+        int row = boardRow;
+        int column;
 
         for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
-            on = !on;
+            //on = !on;
+            boolean squareColorWhite = true;
+
+            out.print(SET_BG_COLOR_DARK_GREY);
+            out.print(SET_TEXT_COLOR_GREEN);
+            if (squareRow == 1) {
+                out.print(numbers[boardRow]+" ");
+                firstRow = false;
+            }
+            else {
+                out.print("  ");
+            }
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                if (on) {
-                    setWhite(out);
+                column = boardCol;
+
+                ChessPosition p = new ChessPosition(row, column);
+
+                if (boardRow % 2 == 0 ) {
+                    if (boardCol % 2 == 0) {
+                        setWhite(out);
+                        squareColorWhite = true;
+                    }
+                    else {
+                        setBlack(out);
+                        squareColorWhite = false;
+                    }
                 }
                 else {
-                    setBlack(out);
+                    if (boardCol % 2 == 0) {
+                        setBlack(out);
+                        squareColorWhite = false;
+                    }
+                    else {
+                        setWhite(out);
+                        squareColorWhite = true;
+                    }
                 }
 
-                on = !on;
+
 
                 if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
+                    //setBlack(out);
                     int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
                     int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
                     out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
+                    printPiece(out, rand.nextBoolean() ? X : O, squareColorWhite);
                     out.print(EMPTY.repeat(suffixLength));
                 }
                 else {
@@ -152,11 +203,11 @@ public class GameClient {
 
                 if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
                     // Draw vertical column separator.
-                    setRed(out);
+                    setGray(out);
                     out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
                 }
 
-                setBlack(out);
+                setGray(out);
             }
 
             out.println();
@@ -169,10 +220,10 @@ public class GameClient {
                 (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
 
         for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setRed(out);
+            setGray(out);
             out.print(EMPTY.repeat(boardSizeInSpaces));
 
-            setBlack(out);
+            setGray(out);
             out.println();
         }
     }
@@ -186,18 +237,28 @@ public class GameClient {
         out.print(SET_BG_COLOR_RED);
         out.print(SET_TEXT_COLOR_RED);
     }
+    private static void setGray(PrintStream out) {
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.print(SET_TEXT_COLOR_DARK_GREY);
+    }
+
 
     private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
+    private static void printPiece(PrintStream out, String player, boolean isWhite) {
+//        out.print(SET_BG_COLOR_WHITE);
+        if (isWhite) {
+            out.print(SET_TEXT_COLOR_BLACK);
+        }
+        else {
+            out.print(SET_TEXT_COLOR_WHITE);
+        }
 
         out.print(player);
 
-        setWhite(out);
+        //setWhite(out);
     }
 }
