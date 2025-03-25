94ebd012ccf38a3ce73b9b7158d8e56032a187f1
dedenys <dedenys.school@gmail.com>
2025-03-25 11:38:50 -0600

    Making test cases register and login

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,23 @@
 package client;
 
+import model.request.LoginRequest;
+import model.request.RegisterRequest;
+import model.result.LoginResult;
+import model.result.RegisterResult;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static int port;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -22,8 +28,106 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerValidTest() {
+
+        String url = "http://localhost:"+String.valueOf(port);
+        System.out.println(url);
+        ServerFacade serverFacade;
+        serverFacade = new ServerFacade(url);
+        RegisterRequest newRequest = new RegisterRequest("newuser", "pass", "email");
+        RegisterResult result = null;
+
+
+        try {
+            serverFacade.clear();
+            result = serverFacade.register(newRequest);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        Assertions.assertNotNull(result);
     }
+
+    @Test
+    public void registerInvalidTest() {
+        String url = "http://localhost:"+String.valueOf(port);
+        System.out.println(url);
+        ServerFacade serverFacade;
+        serverFacade = new ServerFacade(url);
+        RegisterRequest newRequest = new RegisterRequest("newuser", "pass", "email");
+        RegisterResult result = null;
+        try {
+            result = serverFacade.register(newRequest);
+        }
+        catch(Exception e) {
+
+        }
+        Assertions.assertNull(result);
+    }
+
+    @Test
+    public void loginValidTest() {
+        String url = "http://localhost:"+String.valueOf(port);
+        System.out.println(url);
+        ServerFacade serverFacade;
+        serverFacade = new ServerFacade(url);
+
+        RegisterRequest newRegisterRequest = new RegisterRequest("newuser", "pass", "email");
+        RegisterResult resultRegister = null;
+
+        LoginRequest newRequest = new LoginRequest("newuser", "pass");
+        LoginResult result = null;
+        try {
+            serverFacade.clear();
+            serverFacade.register(newRegisterRequest);
+            result = serverFacade.login(newRequest);
+        }
+        catch(Exception e) {
+
+        }
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void loginInvalidTest() {
+        String url = "http://localhost:"+String.valueOf(port);
+        System.out.println(url);
+        ServerFacade serverFacade;
+        serverFacade = new ServerFacade(url);
+        LoginRequest newRequest = new LoginRequest("newuser", "wrongpass");
+        LoginResult result = null;
+        try {
+            result = serverFacade.login(newRequest);
+        }
+        catch(Exception e) {
+
+        }
+        Assertions.assertNull(result);
+    }
+
+    @Test
+    public void clearTest() {
+
+        String url = "http://localhost:"+String.valueOf(port);
+        System.out.println(url);
+        ServerFacade serverFacade;
+        serverFacade = new ServerFacade(url);
+        RegisterRequest newRequest = new RegisterRequest("newuser", "pass", "email");
+        RegisterResult result = null;
+
+        LoginRequest loginRequest = new LoginRequest("newuser", "pass");
+        LoginResult resultLogin = null;
+        try {
+            serverFacade.clear();
+            result = serverFacade.register(newRequest);
+            serverFacade.clear();
+            resultLogin = serverFacade.login(loginRequest);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        Assertions.assertNotNull(result);
+    }
+
 
 }
