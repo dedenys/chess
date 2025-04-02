261a952ea1e1a1eeba0ad06fb92a5463c78f6baf
dedenys <dedenys.school@gmail.com>
2025-04-02 16:21:03 -0600

    Adding server message subclasses

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,32 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+import java.util.Objects;
+
+public class LoadGameMessage extends  ServerMessage {
+
+    private final ChessGame game;
+
+    public LoadGameMessage(ServerMessageType type, ChessGame g) {
+        super(type);
+        game = g;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        LoadGameMessage that = (LoadGameMessage) o;
+        return Objects.equals(game, that.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), game);
+    }
+}
